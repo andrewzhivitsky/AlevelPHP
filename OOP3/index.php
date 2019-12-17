7b5a82d3616be15451485ad532ab7cb7a9ad6f2b
andrewzhivitsky <56889862+andrewzhivitsky@users.noreply.github.com>
2019-12-17 18:50:24 +0200

    Delete index.php

--- OOP3/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-spl_autoload_register('autoloader');
-function autoloader($className){
-    include($className . ".php");
-}
-$car = new landVechicles(755,4, 70, automat);
-$boat = new waterVechicles(1135,0, 23830, automat);
-$plane = new airVechicles(11000,5, 23830, automat);
